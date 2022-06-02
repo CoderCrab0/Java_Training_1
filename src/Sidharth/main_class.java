ebfec7db05665ba1ba35d067373b54a979f95818
sidharth412 <mudigonda.siddhartha41@gmail.com>
2022-06-01 20:56:41 -0700

    3356 test

--- src/Sidharth/main_class.java
+++ src/Sidharth/main_class.java
@@ -4,8 +4,8 @@ public class main_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test");
-		
+		System.out.println("test");
+		System.out.println("test");
 	}
 
 }
